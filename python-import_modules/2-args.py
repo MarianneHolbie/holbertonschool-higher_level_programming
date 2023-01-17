024ed8d7327be88e1e89dd6c8775448d0d0283f7
Arrue Marianne <5608@holbertonstudents.com>
2023-01-17 01:41:33 -0800

    add special case if only 1 arg

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,7 +5,10 @@ if __name__ == "__main__":
         print("0 argument.")
     else:
         index = 1
-        print("{} arguments:".format(len(sys.argv) - 1))
-        while index < len(sys.argv):
-            print("{0}: {1}".format(index, sys.argv[index]))
-            index += 1
+        if len(sys.argv) == 2:
+            print("1 argument: \n{}: {}".format(index, sys.argv[index]))
+        else:
+            print("{} arguments:".format(len(sys.argv) - 1))
+            while index < len(sys.argv):
+                print("{0}: {1}".format(index, sys.argv[index]))
+                index += 1
