d5f18c4dec17d63b1834b9075a613e6d2bca9f50
Arrue Marianne <5608@holbertonstudents.com>
2023-01-17 01:42:54 -0800

    remove one space in print

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,11 +2,11 @@
 if __name__ == "__main__":
     import sys
     if len(sys.argv) == 1:
-        print("0 argument.")
+        print("0 arguments.")
     else:
         index = 1
         if len(sys.argv) == 2:
-            print("1 argument: \n{}: {}".format(index, sys.argv[index]))
+            print("1 argument:\n{}: {}".format(index, sys.argv[index]))
         else:
             print("{} arguments:".format(len(sys.argv) - 1))
             while index < len(sys.argv):
