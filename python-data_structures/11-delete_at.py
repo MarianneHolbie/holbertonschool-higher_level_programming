78c172619eb275907fba5fd58b0b6bd62acde4d9
Arrue Marianne <5608@holbertonstudents.com>
2023-01-23 13:28:36 -0800

    change method and use del method

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    if idx < 0 or idx > len(my_list) - 1:
+    if idx < 0 or idx > len(my_list):
         return (my_list)
     else:
-        new_list = []
-        new_list = my_list[0:idx] + my_list[idx + 1:]
-        return (new_list)
+        del my_list[idx]
+        return (my_list)
