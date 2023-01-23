beebf28f7f264df761fb1a68d1c703e8dffd8a9b
Arrue Marianne <5608@holbertonstudents.com>
2023-01-23 12:02:28 -0800

    initialize programm

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for i in matrix:
-        print(' '.join(map(str, i)))
+    for row in matrix:
+        for val in row:
+            print('{} '.format(val), end=' ')
+        print('\n', end='')
