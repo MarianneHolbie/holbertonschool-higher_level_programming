86c56e272821c852b58e4cf99b29537d17b1d8e5
Arrue Marianne <5608@holbertonstudents.com>
2023-01-24 02:52:51 -0800

    solve pb pep8 format

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -3,4 +3,3 @@ def print_sorted_dictionary(a_dictionary):
     sort_a_dictionary = dict(sorted(a_dictionary.items()))
     for key, value in sort_a_dictionary.items():
         print("{}: {}".format(key, value))
-  
