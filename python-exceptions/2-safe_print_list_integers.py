09e88960da6ba6dbfe5a537e91df84f2939aa576
Arrue Marianne <5608@holbertonstudents.com>
2023-01-24 12:37:33 -0800

    isolate except IndexError and break loop

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -8,7 +8,9 @@ def safe_print_list_integers(my_list=[], x=0):
             count += 1
 
         # except error of type, index : but go through
-        except (TypeError, ValueError, IndexError):
+        except (TypeError, ValueError):
             pass
+        except IndexError:
+            break
     print()
     return (count)
