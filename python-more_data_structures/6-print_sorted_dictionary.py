6cf3d3c3fd30f4cd16ca90fbf1b61d36b2a20898
Arrue Marianne <5608@holbertonstudents.com>
2023-01-24 02:50:42 -0800

    remove unecessary space

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,4 +2,5 @@
 def print_sorted_dictionary(a_dictionary):
     sort_a_dictionary = dict(sorted(a_dictionary.items()))
     for key, value in sort_a_dictionary.items():
-        print("{} : {}".format(key, value))
+        print("{} : {}".format(key, value), end='\n')
+    print()
