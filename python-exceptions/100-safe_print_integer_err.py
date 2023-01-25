e64e7f1cfb570568cbf1745b38b4027eefc077a5
Arrue Marianne <5608@holbertonstudents.com>
2023-01-25 04:45:46 -0800

    import sys, and add file option in print format

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
+import sys
+
+
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
 
     except ValueError as ve:
-        print("Exception: {}".format(ve))
+        print("Exception: {}".format(ve), file=sys.stderr)
         return False
     except TypeError as te:
-        print("Exception: {}".format(te))
+        print("Exception: {}".format(te), file=sys.stderr)
         return False
     else:
         return True
