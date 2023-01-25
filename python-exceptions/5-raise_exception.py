97e7cdb0220fb08866f78b208ecbc9e158c3edfc
Arrue Marianne <5608@holbertonstudents.com>
2023-01-25 01:27:07 -0800

    change string of typeerror

--- python-exceptions/5-raise_exception.py
+++ python-exceptions/5-raise_exception.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def raise_exception():
     try:
-        raise TypeError("Exception raised")
+        raise TypeError("Exception has been raised")
     except TypeError as te:
         print(te)
