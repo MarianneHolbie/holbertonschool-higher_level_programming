5d06cc0b20cebd5462f66c10e7c4d0562aa7b0a4
Arrue Marianne <5608@holbertonstudents.com>
2023-01-25 04:38:03 -0800

    add typeError exception

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -6,5 +6,8 @@ def safe_print_integer_err(value):
     except ValueError as ve:
         print("Exception: {}".format(ve))
         return False
+    except TypeError as te:
+        print("Exception: {}".format(te))
+        return False
     else:
         return True
