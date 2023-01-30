1ab1407f4bcdc60e64e82adf65dec83381691abc
Arrue Marianne <5608@holbertonstudents.com>
2023-01-30 05:01:05 -0800

    implement test for tuple : len, int type, >0

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -52,7 +52,9 @@ class Square():
     @position.setter
     def position(self, value):
         """ Setter method for size"""
-        if type(value) is not tuple:
+        if type(value) is not tuple or len(value) != 2 \
+           or (isinstance(num, int) for num in value) \
+           or (num >= 0 for num in value):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
