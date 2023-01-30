a970d4061a668aa60345ea83a6fc6f3556881f08
Arrue Marianne <5608@holbertonstudents.com>
2023-01-30 01:28:56 -0800

    initialize programm

---
+++ python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" This module is a first creation of class that define a square """
+
+
+class Square():
+    """ 
+    Class that defines a square 
+
+    Attributes
+    ----------
+    __size : size of the square, private attributes
+
+    """
+
+    def __init__(self, __size):
+        __size = None
