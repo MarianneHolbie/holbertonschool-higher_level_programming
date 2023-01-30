f0f38f9503d39eabdb57ba8c676cb7bda8ec6224
Arrue Marianne <5608@holbertonstudents.com>
2023-01-30 04:30:34 -0800

    initialize programm

---
+++ python-classes/5-square.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+""" This module is a first creation of class that define a square
+    with private size"""
+
+
+class Square():
+    """
+    Class that defines a square
+
+    Attributes
+    ----------
+    __size : integer, must be > 0
+        size of the square, private attributes
+     Methods
+    -------
+    area() : calculate the area of the square
+
+    my_print() : print square with #
+    """
+
+    def __init__(self, size=0):
+        """ constructor method """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+        """ Getter method for size"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """ Setter method for size"""
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """ calculate the area of square"""
+        return (self.__size)**2
+
+    def my_print(self):
+        """ print the square with #"""
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                print('#'*self.size)
