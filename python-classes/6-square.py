160e87d09a32a34a6fb27943a7fd9f933e4f5176
Arrue Marianne <5608@holbertonstudents.com>
2023-01-30 05:33:58 -0800

    change print if size =0

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -68,9 +68,9 @@ class Square():
         """ print the square with #"""
         if self.__size == 0:
             print("")
-
-        [print("") for i in range(0, self.__position[1])]
-        for i in range(0, self.__size):
-            [print(" ", end="") for j in range(0, self.__position[0])]
-            [print("#", end="") for k in range(0, self.__size)]
-            print("")
+        else:
+            [print("") for i in range(0, self.__position[1])]
+            for i in range(0, self.__size):
+                [print(" ", end="") for j in range(0, self.__position[0])]
+                [print("#", end="") for k in range(0, self.__size)]
+                print("")
