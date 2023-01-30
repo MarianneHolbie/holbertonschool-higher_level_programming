61e302f6484e8ccf5b13261ef15e512c811d81c1
Arrue Marianne <5608@holbertonstudents.com>
2023-01-30 05:25:20 -0800

    change condition on test of tuple

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -52,11 +52,12 @@ class Square():
     @position.setter
     def position(self, value):
         """ Setter method for size"""
-        if type(value) is not tuple or len(value) != 2 \
-           or all(isinstance(num, int) for num in value) \
-           or all(num >= 0 for num in value):
+        if type(value) is not tuple or len(value) != 2 or \
+           type(value[0]) is not int or value[0] < 0 or\
+           type(value[1]) is not int or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
-        self.__position = value
+        else:
+            self.__position = value
 
     def area(self):
         """ calculate the area of square"""
