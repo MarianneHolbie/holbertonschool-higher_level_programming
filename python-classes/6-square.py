7a3b9d5a8b1ba71caf27fa22b76b19fa14fd1960
Arrue Marianne <5608@holbertonstudents.com>
2023-01-30 07:51:24 -0800

    remove __ before position in init method

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -24,7 +24,7 @@ class Square():
     def __init__(self, size=0, position=(0, 0)):
         """ constructor method """
         self.__size = size
-        self.__position = position
+        self.position = position
 
     @property
     def size(self):
