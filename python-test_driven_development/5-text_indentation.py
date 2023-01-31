36268cb2b0f44ffb1507ca285e49f7519b1eba18
Arrue Marianne <5608@holbertonstudents.com>
2023-01-31 06:53:04 -0800

    initialize programm and test txt file

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+    Module that contain only one function to indentation text
+
+"""
+
+
+def text_indentation(text):
+    """
+    Fonction that print a text with 2 new lines after each of these characters: ., ? and :
+
+    Args :
+        text: string
+
+    Return:
+        indented tring
+
+    Raises:
+        Test TypeError 
+
+    """
+    list_char = [".", "?", ":"]
+    i = 0
+
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    text = text.strip()
+
+    while (i < len(text)):
+
+        if text[i] in list_char:
+            print("{}\n".format(text[i]))
+            if text[i + 1] == " " and i + 1 < len(text):
+                i += 1
+        else:
+            print("{}".format(text[i]), end="")
+        i += 1
