a3251f3f52c1f259e7799e2e0c19f61c50e40a78
Arrue Marianne <5608@holbertonstudents.com>
2023-02-01 01:07:28 -0800

    add property and setter on private instance width and height

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+    Module contain one class to define rectangle
+"""
+
+
+class Rectangle():
+    """
+        Class define a rectangle
+
+        Attribute
+        ---------
+            __width : private instance, width of rectangle
+            __height : private instance, height of rectangle
+
+    """
+
+    def __init__(self, width=0, height=0):
+        self.height = height
+        self.width = width
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__height = value
