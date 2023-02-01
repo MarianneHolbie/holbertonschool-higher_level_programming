aee5cb623d4f35f6826fdfe453ab310eb8c89fe7
Arrue Marianne <5608@holbertonstudents.com>
2023-02-01 06:17:19 -0800

    add public attribute use as symbol to print rectangle

---
+++ python-more_classes/7-rectangle.py
@@ -0,0 +1,95 @@
+#!/usr/bin/python3
+"""
+    Module contain one class to define rectangle
+"""
+
+
+class Rectangle():
+    """
+        Class define a rectangle
+
+        Attribute
+        ---------
+            __width : private instance, width of rectangle
+            __height : private instance, height of the given rectangle
+            number_of_instances : number of instance in the class
+            print_symbol : symbol use to draw rectangle
+
+        Methode
+        --------
+            area : calculate area of the rectangle
+            perimeter: calculate perimeter of the given rectangle
+
+            __str__ : methode to print the rectangle with #
+
+            __repr__ : print widht and height of the rectangle
+
+            __del__ : print message if delate a Rectangle
+
+    """
+
+    number_of_instances = 0
+    print_symbol = '#'
+
+    def __init__(self, width=0, height=0):
+        self.height = height
+        self.width = width
+        Rectangle.number_of_instances += 1
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        """ Function that calculate area of rectangle """
+        return (self.__width * self.__height)
+
+    def perimeter(self):
+        """ Function that calculate perimeter of rectangle """
+        if self.__width == 0 or self.__height == 0:
+            return (0)
+        else:
+            return (2 * (self.__width + self.__height))
+
+    def __str__(self):
+        """ Function that print rectangle with # in string format"""
+        recthashtag = ""
+        if self.__width == 0 or self.__height == 0:
+            return (recthashtag)
+        else:
+            for i in range(self.__height):
+                if i < self.__height - 1:
+                    recthashtag += str(self.print_symbol) * self.__width + "\n"
+                else:
+                    recthashtag += str(self.print_symbol) * self.__width
+        return (recthashtag)
+
+    def __repr__(self):
+        """ Function that implement string in repr function"""
+        return "Rectangle({}, {})".format(self.__width, self.__height)
+
+    def __del__(self):
+        print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
