e56b2a8483a3ea2900da6ad894a6fab999db9b9d
Arrue Marianne <5608@holbertonstudents.com>
2023-02-06 05:10:42 -0800

    add function area empty

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+    Empty class
+"""
+
+
+class BaseGeometry:
+    """
+        class BaseGeometry
+
+        Method
+        ====================
+
+            area : methode not implemented iet
+
+    """
+
+    def area(self):
+        raise Exception('area() is not implemented')
