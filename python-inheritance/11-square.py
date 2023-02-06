d0e5aa2889dc34bfc0bfa8783025241343946ab3
Arrue Marianne <5608@holbertonstudents.com>
2023-02-06 11:01:04 -0800

    initialize programm

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+Write a class Square that inherits from Rectangle.
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+        inherate class of Rectangle
+
+        Attributs
+        ====================
+
+            __size : size of the square
+                private
+
+        Method
+        ====================
+
+            area : calculate area of the rectangle
+    """
+
+    def __init__(self, size):
+        super().integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """
+            calculate area of square
+
+        """
+        result = self.__size ** 2
+        return (result)
+
+    def __str__(self):
+        return ('[Square] {}/{}'.format(self.__size, self.__size))
