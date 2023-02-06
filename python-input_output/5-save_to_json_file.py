c4bbc5366c68e750740f0ebb2ff6db3bbec51100
Arrue Marianne <5608@holbertonstudents.com>
2023-02-06 12:43:43 -0800

    initialize programm

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -10,7 +10,7 @@ import json
 def save_to_json_file(my_obj, filename):
     """
         function that write an Object to a text file
-        usinf JSON representation
+        using JSON representation
 
         Attribut:
         ==================
@@ -21,4 +21,5 @@ def save_to_json_file(my_obj, filename):
     """
 
     with open(filename, "w", encoding="utf-8") as f:
-        json.dump(my_obj, f)
+        object = json.dump(my_obj, f)
+       
