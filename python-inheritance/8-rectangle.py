de52d7e755e46753e757b8fa4a36ae3392ee2721
Arrue Marianne <5608@holbertonstudents.com>
2023-02-06 06:50:00 -0800

    remove definition of class BaseGeometry and implement import module

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,44 +1,8 @@
 #!/usr/bin/python3
 """
-    Module to define class BaseGeometry objects and subclass Rectangle
+Write a class Rectangle that inherits from BaseGeometry.
 """
-
-
-class BaseGeometry:
-    """
-        base class BaseGeometry
-
-        Method
-        ====================
-
-            area : methode not implemented yet
-
-            integer_validator : check value
-
-    """
-
-    def area(self):
-        """
-            find area
-
-        """
-        raise Exception('area() is not implemented')
-
-    def integer_validator(self, name, value):
-        """
-            check value type
-
-            Attributs
-            ===========
-
-                name : string
-                value : value to check
-
-        """
-        if type(value) is not int:
-            raise TypeError('{} must be an integer'.format(name))
-        elif value <= 0:
-            raise ValueError('{} must be greater than 0'.format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
