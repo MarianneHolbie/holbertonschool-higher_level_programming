064349c61a2448b4e2ba3cfd7e1e8d6c6a1b6720
Arrue Marianne <5608@holbertonstudents.com>
2023-02-06 12:56:52 -0800

    initialize programm

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -5,11 +5,11 @@
 
 import json
 
+
 def load_from_json_file(filename):
     """
         function that creates an Object from a JSON file
     """
 
     with open(filename, "r", encoding="utf-8") as f:
-        data = json.load(f)
-        print (data)
+        return (json.load(f))
