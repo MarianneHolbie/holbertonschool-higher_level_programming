09b636675b316b7ebb6fabba3e5aeeb347046459
Arrue Marianne <5608@holbertonstudents.com>
2023-02-06 03:55:45 -0800

    initialize programm & test text file

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,22 @@
+#!/usr/bin/pyhton3
+"""
+    Module that create inheritance class
+"""
+
+
+class MyList(list):
+    """
+        inheritance class of list
+
+        Method
+        ---------------
+
+            print_sorted : print list in ascending sort
+
+    """
+
+    def print_sorted(self):
+        """
+            function that print ascending sort list
+        """
+        print(sorted(self))
