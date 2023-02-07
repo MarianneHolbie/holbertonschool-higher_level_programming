2e250e948d5bee899c341a2c21ef0524e78c8628
Arrue Marianne <5608@holbertonstudents.com>
2023-02-07 01:45:44 -0800

    test if file exist before to not overwrite but append

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -13,18 +13,12 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
-n = len(sys.argv)
 path = Path('./add_item.json')
 
 # if file exist, append and not overwrite
 if path.is_file():
     my_obj = load_from_json_file(filename)
-    if n != 1:
-        for i in range(1, n):
-            my_obj.append(str(sys.argv[i]))
+    my_obj.append(str(sys.argv[1:]))
+    save_to_json_file(my_obj, filename)
 else:
-    my_obj = []
-
-save_to_json_file(my_obj, filename)
-
-load_from_json_file(filename)
+    save_to_json_file(sys.argv[1:], filename)
