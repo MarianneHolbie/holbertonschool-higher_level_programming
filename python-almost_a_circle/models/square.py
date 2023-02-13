2157eb48d60ec8f0712112e68ccf89e56c0c1f8a
Arrue Marianne <5608@holbertonstudents.com>
2023-02-13 05:41:21 -0800

    uptdate function

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -18,6 +18,8 @@ class Square(Rectangle):
         ---------
             __str__: print definition of square
 
+            update : public, assigns attributes
+
     """
 
     def __init__(self, size, x=0, y=0, id=None):
@@ -47,3 +49,29 @@ class Square(Rectangle):
         return ("[Square] ({}) {}/{} - {}".
                 format(self.id, self.x, self.y,
                        self.width))
+
+    def update(self, *args, **kwargs):
+        """
+            function that assigns argument to each attribute
+        """
+        if len(args) != 0:
+            for i in range(len(args)):
+                if i == 0:
+                    self.id = args[0]
+                if i == 1:
+                    self.size = args[1]
+                if i == 2:
+                    self.x = args[2]
+                if i == 3:
+                    self.y = args[3]
+        else:
+            if kwargs is not None:
+                for key, value in kwargs.items():
+                    if key == 'id':
+                        self.id = value
+                    if key == 'size':
+                        self.size = value
+                    if key == 'x':
+                        self.x = value
+                    if key == 'y':
+                        self.y = value
