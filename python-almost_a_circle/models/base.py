1d7e97ed83fa922b38a6a08f02e215b4e70814dd
Arrue Marianne <5608@holbertonstudents.com>
2023-02-13 12:56:01 -0800

    implement also id

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -73,7 +73,7 @@ class Base:
             returns an instance with all attributes already set
         """
         # create dummy form: square 2*2
-        dummy = cls(2, 2, 0, 0)
+        dummy = cls(2, 2, 0, 0, 1)
         # update value
         dummy.update(**dictionary)
         return (dummy)
