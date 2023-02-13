d440e658d9c18e57e1b4dcd86ea97d7d0696bf60
Arrue Marianne <5608@holbertonstudents.com>
2023-02-13 05:13:21 -0800

    change __str__ method

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -8,6 +8,16 @@ from models.rectangle import Rectangle
 class Square(Rectangle):
     """
         subclass of Rectangle
+
+        Attribute
+        ---------
+            size : size of square
+            x andy :position in space of square
+
+        Method
+        ---------
+            __str__: print definition of square
+                   
     """
 
     def __init__(self, size, x=0, y=0, id=None):
@@ -15,3 +25,11 @@ class Square(Rectangle):
             constructeur
         """
         super().__init__(height=size, width=size, x=x, y=y, id=id)
+
+    def __str__(self):
+        """
+            function that print definition of square
+        """
+        return ("[Square] ({}) {}/{} - {}".
+                format(self.id, self.x, self.y,
+                       self.width))
