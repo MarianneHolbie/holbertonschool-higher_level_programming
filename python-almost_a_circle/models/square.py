0283f7d50255890c9dac104ba34a33c7f628200e
Arrue Marianne <5608@holbertonstudents.com>
2023-02-13 05:07:47 -0800

    super init rectangle in square class

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,17 @@
+#!/usr.bin/python3
+"""
+    class Square inherit from Rectangle
+"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """
+        subclass of Rectangle
+    """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """
+            constructeur
+        """
+        super().__init__(height=size, width=size, x=x, y=y, id=id)
