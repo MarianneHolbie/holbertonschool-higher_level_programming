6863c7d8a743c08cec24db3d8002d317ee6963c3
Arrue Marianne <5608@holbertonstudents.com>
2023-02-13 05:51:08 -0800

    add function create dictionary represent rectangle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -26,6 +26,8 @@ class Rectangle(Base):
 
             update: public, assign argument to each attribute
 
+            to_dictionary: return dictionary representation of a Rectangle
+
     """
 
     __nb_objects = 0
@@ -172,3 +174,10 @@ class Rectangle(Base):
                         self.__x = value
                     if key == 'y':
                         self.__y = value
+
+    def to_dictionary(self):
+        """
+            construct dictionary representation of rectangle
+        """
+        return (dict(id=self.id, width=self.__width, height=self.__height,
+                x=self.__x, y=self.__y))
