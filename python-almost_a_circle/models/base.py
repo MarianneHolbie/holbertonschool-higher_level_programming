c657eb87c2b4097d0aebed76ef8bb959a1f4ed40
Arrue Marianne <5608@holbertonstudents.com>
2023-02-13 06:10:00 -0800

    change format of return to str

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -37,4 +37,4 @@ class Base:
         if not list_dictionaries or list_dictionaries is None:
             return ([])
         else:
-            return (json.dumps(list_dictionaries))
+            return (str(json.dumps(list_dictionaries)))
