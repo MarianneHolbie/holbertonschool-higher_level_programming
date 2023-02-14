9aa9408c3ef9a527ae09e407cac9dafa3e96e61d
Arrue Marianne <5608@holbertonstudents.com>
2023-02-14 03:18:51 -0800

    update doc in init file

--- python-almost_a_circle/models/__init__.py
+++ python-almost_a_circle/models/__init__.py
@@ -1,4 +1,34 @@
 """
     initialize package Base class with class and static method
     2 subclass : Rectangle and Square
+    
+    Package Base class
+       ATTRIBUTS
+        ==================
+            __nb_objects : private, count
+
+            id : id of each form
+
+        METHOD
+        ==================
+
+            __init__ : constructor fo Base class
+
+            @classMethod:
+
+                save_to_file : write json string in list_dictionary
+
+                create: create instance of subclasse and update
+                        value with **dict
+
+                load_from_file: load file of instance subclass, extract
+                                form and create
+
+            @StaticMethod:
+
+                to_json_string : static method to return json string
+                                 representation
+
+                from_json_string : return list of the JSON string
+                                   representation json_string
 """
