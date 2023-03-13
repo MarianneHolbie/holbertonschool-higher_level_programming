f3187761c0d775e88e5962211367fe9680d357fc
Arrue Marianne <5608@holbertonstudents.com>
2023-03-13 12:44:04 -0700

    list all shows that have at least one genre linked

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,7 @@
 -- list all show contained in hbtn_0d_tvshows 
+-- FOREIGN KEY (`show_id`) REFERENCES `tv_shows` (`id`)
+-- FOREIGN KEY (`genre_id`) REFERENCES `tv_genres` (`id`)
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows, tv_show_genres 
 WHERE tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_shows.title, tv_show_genres.id ASC;
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
