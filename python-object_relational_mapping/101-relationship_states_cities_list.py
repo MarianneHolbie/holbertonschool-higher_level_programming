5f1ce0ef0ea22c678ee7fcd197d53945f5a4712c
Arrue Marianne <5608@holbertonstudents.com>
2023-03-17 06:08:45 -0700

    add join(City)

--- python-object_relational_mapping/101-relationship_states_cities_list.py
+++ python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -15,7 +15,7 @@ from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 from relationship_city import City
 from relationship_state import Base, State
-from relationship_city import City
+
 
 if __name__ == "__main__":
     # Recover argument from user
@@ -38,7 +38,7 @@ if __name__ == "__main__":
     session = sessionmaker(bind=engine)()
 
     # query : table State with all State by asc order
-    query = session.query(State).order_by(State.id.asc()).all()
+    query = session.query(State).join(City).order_by(State.id.asc()).all()
     # each row contain state.id, state.name, state.cities (linked to City table)
     for state in query:
         print("{}: {}".format(state.id, state.name))
