e50d0da49de869ce2353b812b705b762663328e7
Arrue Marianne <5608@holbertonstudents.com>
2023-04-09 01:48:15 -0700

    add print if argv[2] can t be convert in integer

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -7,6 +7,10 @@ const args = process.argv;
 const str = 'C is fun';
 
 // argv[2] is number time repeat script
-for (let i = 0; i < args[2]; i++) {
-  console.log(str);
+if (parseInt(args[2])) {
+  for (let i = 0; i < args[2]; i++) {
+    console.log(str);
+  }
+} else {
+  console.log('Missing number of occurrences');
 }
