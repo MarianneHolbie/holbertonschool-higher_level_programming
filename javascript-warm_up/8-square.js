3f91b73c9b09bc55712c222ccd6417312913ad1b
Arrue Marianne <5608@holbertonstudents.com>
2023-04-09 01:55:23 -0700

    drawing a square given size

---
+++ javascript-warm_up/8-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+// Importing the process module
+const process = require('process');
+
+const args = process.argv;
+
+// argv[2] is size of square
+if (parseInt(args[2])) {
+  for (let i = 0; i < args[2]; i++) {
+    console.log('x'.repeat(args[2]));
+  }
+} else {
+  console.log('Missing size');
+}
