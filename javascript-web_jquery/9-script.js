ed90e941aeb1cdec3056f32b4170f814f614be6f
Arrue Marianne <5608@holbertonstudents.com>
2023-04-17 06:25:42 -0700

    add execution when DOM is loaded

--- javascript-web_jquery/9-script.js
+++ javascript-web_jquery/9-script.js
@@ -1,5 +1,7 @@
-$.getJSON('https://hellosalut.stefanbohacek.dev/?lang=fr', function (hellosalut) {
-  const value = hellosalut.hello;
+$('document').ready(function () {
+  $.getJSON('https://hellosalut.stefanbohacek.dev/?lang=fr', function (hellosalut) {
+    const value = hellosalut.hello;
 
-  $('DIV#hello').text(value);
+    $('DIV#hello').text(value);
+  });
 });
